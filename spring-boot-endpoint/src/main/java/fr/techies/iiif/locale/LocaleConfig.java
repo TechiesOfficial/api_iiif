396d9c753b775137b131619ce01d7ad5aa23ec81
Nicolas ROBLIN <BNFTT@PTBN514174>
2024-06-20 11:35:19 +0200

    i18n

--- spring-boot-endpoint/src/main/java/fr/techies/iiif/locale/LocaleConfig.java
+++ spring-boot-endpoint/src/main/java/fr/techies/iiif/locale/LocaleConfig.java
@@ -2,8 +2,10 @@ package fr.techies.iiif.locale;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -11,17 +13,4 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @Configuration
 public class LocaleConfig {
 	
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
-	}
-	
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
-		return slr;
-	}
 }
