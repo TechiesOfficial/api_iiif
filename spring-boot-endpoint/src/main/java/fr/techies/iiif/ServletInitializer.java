d6cfe5c1ef9e03238642f9add16ffc773416ad4b
Nicolas ROBLIN <BNF0019370@PDIE4906.pfo.nt.bnf.fr>
2024-06-20 14:41:41 +0200

    Comments

--- spring-boot-endpoint/src/main/java/fr/techies/iiif/ServletInitializer.java
+++ spring-boot-endpoint/src/main/java/fr/techies/iiif/ServletInitializer.java
@@ -3,11 +3,15 @@ package fr.techies.iiif;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Allow to run the project as a war file.
+ * 
+ * The project is <b>NOT</b> intended to be run as an executable file in production.
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(IiifApplication.class);
 	}
-
 }
